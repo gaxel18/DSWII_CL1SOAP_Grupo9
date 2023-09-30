f911c882bb9505b4d4535251bd212eabe10176a6
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-09-30 09:40:26 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL1SOAP_Grupo9/DswiiCl1SoapGrupo9Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_Grupo9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapGrupo9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapGrupo9Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_CL1SOAP_Grupo9/DswiiCl1SoapGrupo9ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL1SOAP_Grupo9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1SoapGrupo9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
